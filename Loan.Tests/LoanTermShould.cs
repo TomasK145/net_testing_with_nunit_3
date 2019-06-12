7da762c494dbdfe59906a93e3fd785b7a4fcb3e5
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-06-12 19:15:52 +0200

    Initial commit

---
+++ Loan.Tests/LoanTermShould.cs
@@ -0,0 +1,26 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loan.Tests
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            //sut --> system under test
+            var loanTerm = new LoanTerm(1);
+
+            Assert.That(loanTerm.ToMonths(), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void StoreYears()
+        {
+            var loanTerm = new LoanTerm(1);
+
+            Assert.That(loanTerm.Years, Is.EqualTo(1));
+        }
+    }
+}
